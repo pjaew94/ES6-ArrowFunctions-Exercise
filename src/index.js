c2d7983cdeeacfa8741afb2c2ccc59f06a5656ef
pjaew94 <55684001+pjaew94@users.noreply.github.com>
2019-12-16 21:53:39 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+var numbers = [3, 56, 2, 48, 5];
+
+
+// const newNumbers = numbers.map(x => x*2)
+
+
+// const newNumbers = numbers.filter(num => num < 10)
+
+// var newNumber = numbers.reduce((accumulator, currentNumber) => accumulator + currentNumber)
+
+
+// const newNumber = numbers.find(num => num > 10)
+
+
+// const newNumber = numbers.findIndex(num => num > 10)
+
+
